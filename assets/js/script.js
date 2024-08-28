837d50c0716ef59ef3499f94fde981d972ab27e5
Izulsyauki Imani <izulsyaukiimani@gmail.com>
2024-08-29 06:00:50 +0700

    Day 3 - Add Form and Alert JS

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+const textarea = document.querySelector("textarea");
+textarea.addEventListener("keyup", (event) => {
+  textarea.style.height = "102px";
+  let scHeight = event.target.scrollHeight;
+  textarea.style.height = `${scHeight}px`;
+});
+
+function sendEmail(event){
+  event.preventDefault();
+  const myEmail = "izulsyaukiimani@gmail.com";
+
+  const inputName = document.getElementById("fullname").value;
+  const inputEmail = document.getElementById("email").value;
+  const inputPhone = document.getElementById("phone-number").value;
+  const inputRole = document.getElementById("role").value;
+  const inputMsg = document.getElementById("message").value;
+
+  const mailtoLink = `mailto:${myEmail}?subject=${encodeURIComponent(inputRole)}&body=${encodeURIComponent(`Name: ${inputName}\nPhone Number: ${inputPhone}\nMessage: ${inputMsg}`)}`;
+
+  window.location.href = mailtoLink;
+
+  alert("Success to input your data, and now continue with mail");
+
+  const contact = {
+    name: inputName,
+    email: inputEmail,
+    phoneNumber: inputPhone,
+    subject: inputRole,
+    message: inputMsg,
+  };
+
+  console.log(contact);
+}
