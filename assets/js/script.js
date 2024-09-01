2fd569207677aecef5d8deac6b59bf8930f8febe
Izulsyauki Imani <izulsyaukiimani@gmail.com>
2024-09-01 21:31:47 +0700

    Day 5 - Adding function Submit and Delete

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,4 +45,90 @@ document.getElementById("upload-img").addEventListener("change", function () {
   label.textContent = fileName; // Mengubah teks label menjadi nama file
 });
 
-// 
+// Adding project
+
+
+
+function addProject(event) {
+  event.preventDefault();
+
+  const title = document.getElementById("input-title").value;
+  const startDate = document.getElementById("input-start-date").value;
+  const endDate = document.getElementById("input-end-date").value;
+  const description = document.getElementById("input-description").value;
+
+  // Mengecek start date harus lebih besar dari end date
+  if (new Date(endDate) < new Date(startDate)) {
+    alert("End Date harus lebih besar dari Start Date!");
+    return;
+  }
+
+  // Menghitung durasi proyek
+  const duration = calcDuration(new Date(startDate), new Date(endDate));
+
+  // Mengambil nilai cekbox
+  const tech = [];
+  for (let i = 1; i <= 6; i++) {
+    const checkbox = document.getElementById(`stack${i}`);
+    if (checkbox.checked) {
+      tech.push(checkbox.nextElementSibling.textContent);
+    }
+  }
+
+  // Mengambil gambar yang di upload
+  const imgFile = document.getElementById("upload-img").files[0];
+  const imgURL = URL.createObjectURL(imgFile);
+
+  const addProjectPost = document.querySelector(".project-post");
+  const newProjectPost = `
+        <div class="project-card">
+          <img
+            src="${imgURL}"
+            alt=""
+          />
+  
+          <div class="project-content">
+            <div class="title">
+            <h1>${title}</h1>
+            <p>Duration: ${duration}</p>
+            </div>
+
+            <p id="description">
+              ${description}
+            </p>
+          </div>
+
+          <div class="stack-label">
+            ${tech.map((tech) => `<label>${tech}</label>`).join("")}
+          </div>
+  
+          <div class="btn-prj">
+            <button class="edit">Edit</button>
+            <button class="delete">Delete</button>
+          </div>
+        </div>
+        `;
+
+  // Add project card baru ke dalam div project post
+  addProjectPost.insertAdjacentHTML("afterbegin", newProjectPost);
+
+  // // Menghapus card project post
+  const deleteButtons = addProjectPost.querySelectorAll(".btn-prj .delete");
+  deleteButtons.forEach(button => {
+    button.addEventListener('click', function() {
+      this.parentElement.parentElement.remove();
+    });
+  });
+
+  // Mengosongkan form setelah submit
+  document.querySelector("form").reset();
+  document.getElementById("upload-img").value = "";
+}
+
+function calcDuration(startDate, endDate) {
+  const calcTime = Math.abs(endDate - startDate);
+  const calcDays = Math.ceil(calcTime / (1000 * 60 * 60 * 24));
+  const months = Math.floor(calcDays / 30);
+  const days = calcDays % 30;
+  return `${months} month(s) ${days} days{s}`;
+}
