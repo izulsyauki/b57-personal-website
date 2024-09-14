1734bd866691a9c420c32f1cb9b234377cf40bf9
Izulsyauki Imani <izulsyaukiimani@gmail.com>
2024-09-14 10:41:59 +0700

    Day 14 - CRUD Project Field Done

--- index.js
+++ index.js
@@ -9,34 +9,34 @@ const config = require("./config/config.json");
 const { Sequelize, QueryTypes } = require("sequelize");
 const sequelize = new Sequelize(config.development);
 const Project = require("./models").
-projects;
+	projects;
 
 
 const techData = [
-  {
-    name : 'Node.Js',
-    key : 'node',
-  },
-  {
-    name : 'Express.Js',
-    key : 'express',
-  },
-  {
-    name : 'React.Js',
-    key : 'react',
-  },
-  {
-    name : 'Next.Js',
-    key : 'next',
-  },
-  {
-    name : 'Typescript',
-    key : 'typescript',
-  },
-  {
-    name : 'Others',
-    key : 'others',
-  }
+	{
+		name: 'Node.Js',
+		key: 'node',
+	},
+	{
+		name: 'Express.Js',
+		key: 'express',
+	},
+	{
+		name: 'React.Js',
+		key: 'react',
+	},
+	{
+		name: 'Next.Js',
+		key: 'next',
+	},
+	{
+		name: 'Typescript',
+		key: 'typescript',
+	},
+	{
+		name: 'Others',
+		key: 'others',
+	}
 ];
 
 
@@ -47,14 +47,12 @@ app.use("/assets", express.static(path.join(__dirname, "assets")));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json()); // data untuk parsing objek
 hbs.registerPartials(path.join(__dirname, "views", "partials"));
-hbs.registerHelper('isExist', function(array, value){
-  console.log('array', array);
-  console.log('tech', value);
-  return array.includes(value);
+hbs.registerHelper('isExist', function (array, value) {
+	return array.includes(value);
 });
-hbs.registerHelper('getTechName', function(value){
-  const result = techData.find(tech => tech.key === value);
-  return result ? result.name : '';
+hbs.registerHelper('getTechName', function (value) {
+	const result = techData.find(tech => tech.key === value);
+	return result ? result.name : '';
 })
 
 // Routing html
@@ -69,105 +67,114 @@ app.get("/contact-me", contactMe);
 app.get("/testimoni", testimoni);
 
 async function home(req, res) {
-  const result = await Project.findAll();
+	const result = await Project.findAll();
 
-  res.render("index", { result });
+	res.render("index", { result });
 }
 
 async function addProject(req, res) {
-  const tech = techData;
-  res.render("add-project", {tech});
+	const tech = techData;
+	res.render("add-project", { tech });
 }
 
 async function addProjectPost(req, res) {
-  // mengambil data dari form
-  const { inputTitle, startDate, endDate, technologies, description } =
-    req.body;
+	// mengambil data dari form
+	const { inputTitle, startDate, endDate, technologies, description } =
+		req.body;
 
-  const duration = calcProjectDuration(startDate, endDate);
+	const duration = calcProjectDuration(startDate, endDate);
 
-  console.log('technologies type', technologies);
+	await Project.create({
+		title: inputTitle,
+		startDate: startDate,
+		endDate: endDate,
+		technologies: technologies,
+		description: description,
+		image: 'https://images.pexels.com/photos/3183183/pexels-photo-3183183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+		duration: duration,
+	});
 
-  await Project.create({
-    title: inputTitle,
-    startDate: startDate,
-    endDate: endDate,
-    technologies: technologies,
-    description: description,
-    image: 'https://images.pexels.com/photos/3183183/pexels-photo-3183183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    duration: duration,
-  });
-
-  console.log("duration:", duration)
-  console.log("technologies", technologies)
-  res.redirect("/");
+	res.redirect("/");
 }
 
 async function editProjectView(req, res) {
-  const { id } = req.params;
+	const { id } = req.params;
 
-  // find projek berdasarkan id
-  const query = `SELECT * FROM projects WHERE id=${id}`;
-  const result = await sequelize.query(query, { type: QueryTypes.SELECT });
+	const result = await Project.findOne({
+		where: {
+			id: id,
+		}
+	});
 
-  const [project] = result;
-  const tech = techData;
-  
-  if (!project) return res.status(404).send("Project not found");
+	const tech = techData;
 
-  res.render("edit-project", {project, tech});
+	if (!result) return res.status(404).send("Project not found");
+
+	res.render("edit-project", { result, tech });
 }
 
 async function editProject(req, res) {
-  const { id } = req.params;
-  const { inputTitle, startDate, endDate, technologies, description } =
-    req.body;
+	const { id } = req.params;
+	const { inputTitle, startDate, endDate, technologies, description } =
+		req.body;
 
-  const duration = calcProjectDuration(startDate, endDate);
+	const duration = calcProjectDuration(startDate, endDate);
 
-  const query = `UPDATE projects SET title='${inputTitle}', "startDate"='${startDate}', "endDate"='${endDate}', technologies='{${technologies}}', description='${description}', duration='${duration}'`
+	const project = await Project.findOne({
+		where: {
+			id: id
+		}
+	});
 
+	if (!project) return res.status(404).send("Project not found");
 
-  if (!id) return res.status(404).send("Project not found");
-  
-  const result = await sequelize.query(query, {type: QueryTypes.UPDATE})
-  res.redirect("/");
+	project.title = inputTitle;
+	project.startDate = startDate;
+	project.endDate = endDate;
+	project.technologies = technologies;
+	project.description = description;
+	project.image = 'https://images.pexels.com/photos/3183183/pexels-photo-3183183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
+	project.duration = duration;
+
+	await project.save();
+
+	res.redirect("/");
 }
 
 async function deleteProject(req, res) {
-  const { id } = req.params;
-  let query = `SELECT * FROM projects WHERE id=${id}`;
-  let result = await sequelize.query(query, { type: QueryTypes.SELECT });
+	const { id } = req.params;
+	let query = `SELECT * FROM projects WHERE id=${id}`;
+	let result = await sequelize.query(query, { type: QueryTypes.SELECT });
 
-  if (!result) return res.status(404).send("Project not found");
-  query = `DELETE FROM projects WHERE id=${id}`;
-  result = await sequelize.query(query, { type: QueryTypes.DELETE });
-  res.redirect("/");
+	if (!result) return res.status(404).send("Project not found");
+	query = `DELETE FROM projects WHERE id=${id}`;
+	result = await sequelize.query(query, { type: QueryTypes.DELETE });
+	res.redirect("/");
 }
 
 async function detailProject(req, res) {
-  const { id } = req.params;
-  // query select untuk mengambil data dari db
-  const query = `SELECT * FROM public.projects WHERE id=${id}`;
-  const result = await sequelize.query(query, { type: QueryTypes.SELECT });
+	const { id } = req.params;
+	// query select untuk mengambil data dari db
+	const query = `SELECT * FROM public.projects WHERE id=${id}`;
+	const result = await sequelize.query(query, { type: QueryTypes.SELECT });
 
-  const [project] = result;
+	const [project] = result;
 
-  if (!result) {
-    return res.status(404).send("Project not found");
-  }
+	if (!result) {
+		return res.status(404).send("Project not found");
+	}
 
-  res.render("detail-project", { project });
+	res.render("detail-project", { project });
 }
 
 function contactMe(req, res) {
-  res.render("get-in-touch");
+	res.render("get-in-touch");
 }
 
 function testimoni(req, res) {
-  res.render("testimoni");
+	res.render("testimoni");
 }
 
 app.listen(port, () => {
-  console.log(`Server sedang berjalan di port ${port}`);
+	console.log(`Server sedang berjalan di port ${port}`);
 });
