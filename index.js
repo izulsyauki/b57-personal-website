9a20acaa9b38c70cda7bf0d34cdc81f430e9c228
Izulsyauki Imani <izulsyaukiimani@gmail.com>
2024-09-18 10:57:54 +0700

    Day 17 - Adding vercel.json again

--- index.js
+++ index.js
@@ -4,94 +4,90 @@ const hbs = require("hbs");
 const app = express();
 const port = 5000;
 const { calcProjectDuration } = require("./assets/js/utils");
-const Project = require("./models").
-	projects;
-const User = require("./models").
-	users;
-const bcrypt = require('bcrypt');
-const session = require('express-session');
-const flash = require('express-flash');
-const cookieParser = require('cookie-parser');
-const moment = require('moment');
-const multer = require('multer');
+const Project = require("./models").projects;
+const User = require("./models").users;
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const flash = require("express-flash");
+const cookieParser = require("cookie-parser");
+const moment = require("moment");
+const multer = require("multer");
 // setting multer
 const storage = multer.diskStorage({
-	destination: function (req, file, cb){
-		cb(null, './assets/images/');
-	},
-	filename: function(req, file, cb){
-		cb(null, file.originalname);
-	}
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+	const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1E9);
+    cb(null, file.originalname);
+  },
 });
-const upload = multer ({ storage });
+const upload = multer({ storage });
 
 // data untuk cekbox
 const techData = [
-	{
-		name: 'Node.Js',
-		key: 'node',
-	},
-	{
-		name: 'Express.Js',
-		key: 'express',
-	},
-	{
-		name: 'React.Js',
-		key: 'react',
-	},
-	{
-		name: 'Next.Js',
-		key: 'next',
-	},
-	{
-		name: 'Typescript',
-		key: 'typescript',
-	},
-	{
-		name: 'Others',
-		key: 'others',
-	}
+  {
+    name: "Node.Js",
+    key: "node",
+  },
+  {
+    name: "Express.Js",
+    key: "express",
+  },
+  {
+    name: "React.Js",
+    key: "react",
+  },
+  {
+    name: "Next.Js",
+    key: "next",
+  },
+  {
+    name: "Typescript",
+    key: "typescript",
+  },
+  {
+    name: "Others",
+    key: "others",
+  },
 ];
 
-
-// setting middleware
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views"));
 app.use("/assets", express.static(path.join(__dirname, "assets")));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json()); // data untuk parsing objek
-app.use(session({
-	name: 'my-session',
-	secret: 'u94eSa7gzu',
-	resave: false,
-	saveUninitialized: true,
-	cookie: {
-		maxAge: 1000 * 60 * 60 * 24,
-		secure: false
-	}
-})
+app.use(
+  session({
+    name: "my-session",
+    secret: "u94eSa7gzu",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+      secure: false,
+    },
+  })
 );
 app.use(flash());
 app.use(cookieParser());
 
 // setting helpers
 hbs.registerPartials(path.join(__dirname, "views", "partials"));
-hbs.registerHelper('isExist', function (array, value) {
-	return array.includes(value);
+hbs.registerHelper("isExist", function (array, value) {
+  return array.includes(value);
 });
-hbs.registerHelper('getTechName', function (value) {
-	const result = techData.find(tech => tech.key === value);
-	return result ? result.name : '';
+hbs.registerHelper("getTechName", function (value) {
+  const result = techData.find((tech) => tech.key === value);
+  return result ? result.name : "";
 });
-hbs.registerHelper('formatDate', function(date){
-	return moment(date).format('YYYY-MM-DD');
-})
-
-
+hbs.registerHelper("formatDate", function (date) {
+  return moment(date).format("YYYY-MM-DD");
+});
 
 // Routing html
 app.get("/", home);
-app.get("/add-project", addProject);
+app.get("/add-project", addProjectView);
 app.post("/add-project", upload.single("uploadImage"), addProjectPost);
 app.get("/edit-project/:id", editProjectView);
 app.post("/edit-project/:id", upload.single("uploadImage"), editProject);
@@ -105,278 +101,285 @@ app.get("/login", loginView);
 app.post("/login", login);
 app.get("/logout", logout);
 
-
 async function home(req, res) {
-	const result = await Project.findAll({
-		order: [
-			['createdAt', 'DESC']
-		]
-	});
-	const user = req.session.user;
-	
-	const resultWithUser = result.map(item => ({
-		...item.dataValues,
-		user: user,
-	}))
+  const result = await Project.findAll({
+    order: [["createdAt", "DESC"]],
+  });
+  const user = req.session.user;
 
-	res.render("index", { result: resultWithUser, user});
+  const resultWithUser = result.map((item) => ({
+    ...item.dataValues,
+    user: user,
+  }));
+
+  res.render("index", { result: resultWithUser, user });
 }
 
-async function addProject(req, res) {
-	const tech = techData;
-	const user = req.session.user;
+async function addProjectView(req, res) {
+  try {
+    const tech = techData;
+    const user = req.session.user;
 
-	console.log("project?", req.session.user.id);
-	res.render("add-project", { tech, user });
+    if (!user) {
+      res.cookie("warning", "You're must login to continue!", {
+        httpOnly: true,
+        maxAge: 5000,
+      });
+      return res.redirect("login");
+    }
+
+    res.render("add-project", { tech, user });
+  } catch (error) {
+    req.flash("error", "Something went wrong!");
+    return res.redirect("/");
+  }
 }
 
 async function addProjectPost(req, res) {
-	try {
-		console.log('request file ',req.file)
-		const { inputTitle, startDate, endDate, technologies, description, } =
-			req.body;
+  try {
+    console.log("request file ", req.file);
+    const { inputTitle, startDate, endDate, technologies, description } =
+      req.body;
 
-		const duration = calcProjectDuration(startDate, endDate);
+    const duration = calcProjectDuration(startDate, endDate);
 
-		await Project.create({
-			title: inputTitle,
-			startDate: startDate,
-			endDate: endDate,
-			technologies: technologies,
-			description: description,
-			image: "/" + req.file.path,
-			duration: duration,
-			userId: req.session.user.id
-		});
+    await Project.create({
+      title: inputTitle,
+      startDate: startDate,
+      endDate: endDate,
+      technologies: technologies,
+      description: description,
+      image: "/" + req.file.path,
+      duration: duration,
+      userId: req.session.user.id,
+    });
 
-		req.flash("success", "Adding Project Successful!");
-		res.redirect("/");
-	} catch (error) {
-		req.flash("error", "Something went wrong!")
-		return res.redirect("/");
-	}
+    req.flash("success", "Adding project successful!");
+    res.redirect("/");
+  } catch (error) {
+    req.flash("error", "Something went wrong!");
+    return res.redirect("/");
+  }
 }
 
 async function editProjectView(req, res) {
-	try {
-		const { id } = req.params;
-		const user = req.session.user;
+  try {
+    const { id } = req.params;
+    const user = req.session.user;
 
-		const result = await Project.findOne({
-			where: {
-				id: id,
-			}
-		});
+    const result = await Project.findOne({
+      where: {
+        id: id,
+      },
+    });
 
-		const tech = techData;
+    const tech = techData;
 
-		if (!result) {
-			req.flash("error", "Project not found");
-			return res.redirect("/", user)
-		}
+    if (!result) {
+      req.flash("error", "Project not found");
+      return res.redirect("/", user);
+    }
 
-		res.render("edit-project", { result, tech });
-	} catch (error) {
-		req.flash("error", "Something went wrong!")
-		return res.redirect("/");
-	}
+    res.render("edit-project", { result, tech });
+  } catch (error) {
+    req.flash("error", "Something went wrong!");
+    return res.redirect("/");
+  }
 }
 
 async function editProject(req, res) {
-	try {
-		console.log('request file', req.file);
-		const { id } = req.params;
-		const { inputTitle, startDate, endDate, technologies, description } =
-			req.body;
+  try {
+    console.log("request file", req.file);
+    const { id } = req.params;
+    const { inputTitle, startDate, endDate, technologies, description } =
+      req.body;
 
-		const duration = calcProjectDuration(startDate, endDate);
+    const duration = calcProjectDuration(startDate, endDate);
 
-		const project = await Project.findOne({
-			where: {
-				id: id
-			}
-		});
+    const project = await Project.findOne({
+      where: {
+        id: id,
+      },
+    });
 
-		if (!project) {
-			req.flash("error", "Project not found");
-			return res.redirect("/");
-		}
+    if (!project) {
+      req.flash("error", "Project not found");
+      return res.redirect("/");
+    }
 
-		project.title = inputTitle;
-		project.startDate = startDate;
-		project.endDate = endDate;
-		project.technologies = technologies;
-		project.description = description;
-		project.image = "/" + req.file.path;
-		project.duration = duration;
-		
-		await project.save();
+    project.title = inputTitle;
+    project.startDate = startDate;
+    project.endDate = endDate;
+    project.technologies = technologies;
+    project.description = description;
+    project.image = "/" + req.file.path;
+    project.duration = duration;
 
-		req.flash("success", "Edit Successfull!")
-		res.redirect("/");
-	} catch (error) {
-		console.log('ini error nya bang '+ error);
-		req.flash("error", "Something went wrong!")
-		return res.redirect("/");
-	}
+    await project.save();
+
+    req.flash("success", "Edit successfull!");
+    res.redirect("/");
+  } catch (error) {
+    req.flash("error", "Something went wrong!");
+    return res.redirect("/");
+  }
 }
 
 async function deleteProject(req, res) {
-	try {
-		const { id } = req.params;
-		let result = await Project.findOne({
-			where: {
-				id: id
-			}
-		});
+  try {
+    const { id } = req.params;
+    let result = await Project.findOne({
+      where: {
+        id: id,
+      },
+    });
 
-		if (!result) {
-			req.flash("error", "Project not found");
-			return res.redirect("/");
-		}
+    if (!result) {
+      req.flash("error", "Project not found");
+      return res.redirect("/");
+    }
 
-		await Project.destroy({
-			where: {
-				id: id
-			}
-		});
-		res.redirect("/");
-	} catch (error) {
-		req.flash("error", "Something went wrong!")
-		return res.redirect("/");
-	}
+    await Project.destroy({
+      where: {
+        id: id,
+      },
+    });
+    res.redirect("/");
+  } catch (error) {
+    req.flash("error", "Something went wrong!");
+    return res.redirect("/");
+  }
 }
 
 async function detailProject(req, res) {
-	try {
-		const { id } = req.params;
-		const user = req.session.user;
+  try {
+    const { id } = req.params;
+    const user = req.session.user;
 
+    const result = await Project.findOne({
+      where: {
+        id: id,
+      },
+      include: [
+        {
+          model: User,
+          as: "user",
+          attributes: ["id", "name", "email"],
+        },
+      ],
+    });
 
-		const result = await Project.findOne({
-			where: {
-				id: id,
-			},
-			include :[
-				{
-					model: User,
-					as: 'user',
-					attributes: ['id', 'name','email']
-				}
-			]
-		});
+    if (!result) {
+      req.flash("error", "Project not found");
+      return res.redirect("/");
+    }
 
-		if (!result) {
-			req.flash("error", "Project not found")
-			return res.redirect("/")
-		}
-
-		console.log("kamu apa bang?",result.image);
-		res.render("detail-project", { result, user });
-	} catch (error) {
-		console.log(error);
-		req.flash("error", "Something went wrong!")
-		return res.redirect("/");
-	}
+    console.log("kamu apa bang?", result.image);
+    res.render("detail-project", { result, user });
+  } catch (error) {
+    console.log(error);
+    req.flash("error", "Something went wrong!");
+    return res.redirect("/");
+  }
 }
 
 function contactMe(req, res) {
-	const user = req.session.user;
-	res.render("get-in-touch", { user });
+  const user = req.session.user;
+  res.render("get-in-touch", { user });
 }
 
 function testimoni(req, res) {
-	const user = req.session.user;
-	res.render("testimoni", { user });
+  const user = req.session.user;
+  res.render("testimoni", { user });
 }
 
 function registerView(req, res) {
-	res.render("register");
+  res.render("register");
 }
 
 async function register(req, res) {
-	try {
-		const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-		const saltRounds = 10;
-		const hashedPass = await bcrypt.hash(password, saltRounds);
+    const saltRounds = 10;
+    const hashedPass = await bcrypt.hash(password, saltRounds);
 
-		await User.create({
-			name: name,
-			email: email,
-			password: hashedPass
-		});
+    await User.create({
+      name: name,
+      email: email,
+      password: hashedPass,
+    });
 
-		req.flash("success", "Register Successful!")
-		res.redirect("login");
-	} catch (error) {
-		req.flash("error", "Something went wrong!")
-		return res.redirect("register");
-	}
+    req.flash("success", "Register Successful!");
+    res.redirect("login");
+  } catch (error) {
+    req.flash("error", "Something went wrong!");
+    return res.redirect("register");
+  }
 }
 
 function loginView(req, res) {
-	const flashMessage = req.cookies.flash_message;
-	res.clearCookie("flash_message")
+  const messageWarning = req.cookies.warning;
+  res.clearCookie("warning");
 
-	res.render("login", { flashMessage });
+  res.render("login", { messageWarning });
 }
 
 async function login(req, res) {
-	try {
-		const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-		const user = await User.findOne({
-			where: {
-				email: email,
-			}
-		});
+    const user = await User.findOne({
+      where: {
+        email: email,
+      },
+    });
 
-		if (!user) {
-			req.flash("error", "User not found");
-			return res.redirect("/login");
-		}
+    if (!user) {
+      req.flash("error", "User not found");
+      return res.redirect("/login");
+    }
 
-		const isValidPass = await bcrypt.compare(password, user.password)
+    const isValidPass = await bcrypt.compare(password, user.password);
 
-		if (!isValidPass) {
-			req.flash("error", "Check again youre email or password");
-			return res.redirect("/login");
-		}
+    if (!isValidPass) {
+      req.flash("error", "Check again youre email or password");
+      return res.redirect("/login");
+    }
 
-		// menyimpan data user tanpa password ke session
-		const userWithoutPass = { ...user.get(), password: undefined };
-		req.session.user = userWithoutPass
+    // menyimpan data user tanpa password ke session
+    const userWithoutPass = { ...user.get(), password: undefined };
+    req.session.user = userWithoutPass;
 
-		req.flash("success", "Login Successful!")
-		res.redirect("/");
-	} catch (error) {
-		req.flash("error", "Something went wrong")
-		res.redirect("/login");
-	}
+    req.flash("success", "Login Successful!");
+    res.redirect("/");
+  } catch (error) {
+    req.flash("error", "Something went wrong");
+    res.redirect("/login");
+  }
 }
 
 function logout(req, res) {
-	try {
-		res.cookie("flash_message", "You're Logged out, Please Login to Continue!", {
-			httpOnly: true,
-			maxAge: 5000,
-		});
+  try {
+    res.cookie("warning", "You're Logged out, Please Login to Continue!", {
+      httpOnly: true,
+      maxAge: 5000,
+    });
 
-		req.session.destroy((err) => {
-			if (err) {
-				req.flash("error", "Logout failed, Try again!");
-				return res.redirect("/");
-			}
+    req.session.destroy((err) => {
+      if (err) {
+        req.flash("error", "Logout failed, Try again!");
+        return res.redirect("/");
+      }
 
-			res.redirect("/login");
-		});
-	} catch (error) {
-		req.flash("error", "Something went wrong")
-		res.redirect("/");
-	}
+      res.redirect("/login");
+    });
+  } catch (error) {
+    req.flash("error", "Something went wrong");
+    res.redirect("/");
+  }
 }
 
 app.listen(port, () => {
-	console.log(`Server sedang berjalan di port ${port}`);
+  console.log(`Server sedang berjalan di port ${port}`);
 });
