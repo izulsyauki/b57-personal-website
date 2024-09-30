ab3eac7b9744e01a0bb5e1dd5b6118c834612a2a
Izulsyauki Imani <izulsyaukiimani@gmail.com>
2024-09-30 12:51:15 +0700

    Fixing env for cloudinary

--- index.js
+++ index.js
@@ -327,8 +327,7 @@ async function deleteProject(req, res) {
 			res.redirect("/");
 		});
 	} catch (error) {
-		console.log("Error delete bang, ", error);
-		req.flash("error", "Something went wrong!", error);
+		req.flash("error", "Something went wrong!");
 		return res.redirect("/");
 	}
 }
